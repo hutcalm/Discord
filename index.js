f18b6199f383e15cf9c88ef073a77c10beb6ed21
hutcalm <hutcalm@gmail.com>
2024-08-03 09:30:53 +0330

    Update index.js

--- index.js
+++ index.js
@@ -23,19 +23,19 @@ client.on('ready', async () => {
   console.log(`${client.user.tag} - rich presence started!`);
 
   const r = new Discord.RichPresence()
-    .setApplicationId('1146570151253266432')
+    .setApplicationId('1269170112418545674')
     .setType('STREAMING')
     .setURL('https://www.twitch.tv/hutcalm') //Must be a youtube video link 
-    .setState('Fate')
-    .setName('Yamete Kudasai')
-    .setDetails(`Me`)
+    .setState('Hamster Kombat')
+    .setName('Click Click Click')
+    .setDetails(`HeH Life`)
     .setStartTimestamp()
- .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1016707563594715267/1268817882570162269/tumblr_ngsnsj6cFe1s53g98o1_1280.gif?ex=66adce3c&is=66ac7cbc&hm=44538c5b7db68e478360f4a188a30689d118feed64e5f9f0b6599cc1ce2cfd28&') //You can put links in tenor or discord and etc.
-    .setAssetsLargeText('Yamete Kudasai') //Text when you hover the Large image
-    .setAssetsSmallImage('https://cdn.discordapp.com/attachments/1016707563594715267/1268817570254028841/animesher.com_girl-black-gumi-980940.gif?ex=66adcdf2&is=66ac7c72&hm=736028ab09419d527e22e1e9c12f6a770bd0bf01e5be09f3e16e7ffb8ea58477&') //You can put links in tenor or discord and etc.
-    .setAssetsSmallText('oi') //Text when you hover the Small image
-    .addButton('Discord', 'https://www.youtube.com/@snsenpai')
-    .addButton('Youtube', 'https://www.youtube.com/@snsenpai');
+ .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1016707563594715267/1269169284567928842/f5f27448c036af645c27467c789ad759.gif?ex=66af1581&is=66adc401&hm=47844de545e93b036e0b8daed726fa5f0b5c558a460a6321df4d5adf139fa5d3&') //You can put links in tenor or discord and etc.
+    .setAssetsLargeText('Hamster Kombat') //Text when you hover the Large image
+    .setAssetsSmallImage('https://cdn.discordapp.com/attachments/1016707563594715267/1269169283804561459/cherry-blossom.gif?ex=66af1581&is=66adc401&hm=a05f85ad7995ea183eb449d7bd55e97d787a553b4b0bcdb86c03e92d4c9241ea&') //You can put links in tenor or discord and etc.
+    .setAssetsSmallText('Ara') //Text when you hover the Small image
+    .addButton('Discord', 'https://www.youtube.com/@hutcalm')
+    .addButton('Youtube', 'https://www.youtube.com/@hutcalm');
 
   client.user.setActivity(r);
   client.user.setPresence({ status: "idle" }); //dnd, online, idle, offline
@@ -44,7 +44,7 @@ client.on('ready', async () => {
   setInterval(() => {
     const newTime = formatTime();
     if (newTime !== prevTime) {
-      const newDetails = `Me`;
+      const newDetails = `Life`;
       r.setDetails(newDetails);
       client.user.setActivity(r);
       prevTime = newTime;
